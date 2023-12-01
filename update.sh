617193f23a7abe5c1f209efead62d4655451a2f7
LiaoYuanF <fengliaoyuan@outlook.com>
2023-12-01 18:05:58 +0800

    update hexo source

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ echo "begin deploy"
 cd /root/blog/
 hexo d -g
 echo "begin backup"
-cp -rf /root/blog/ /root/LiaoYuanF.github.io/
+cp -rf /root/blog/* /root/LiaoYuanF.github.io/
 cd /root/LiaoYuanF.github.io/
 git add .
 git commit -m "update hexo source"
